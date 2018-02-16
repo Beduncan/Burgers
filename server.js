45d2a319311f1579410c32b22399c3547faf0330
bryantduncan98@gmail.comgit config --global user.email bryantduncan98@gmail.com <bryantduncan98@gmail.com>
2018-02-16 11:54:24 -0700

    adding css

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ var PORT = process.env.PORT || 3000;
 // set up the express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+//accessing the public folder for css
+app.use(express.static(process.cwd() + "/public"));
+
 
 //set up handlebars
 var exphbs = require("express-handlebars");
@@ -18,6 +21,7 @@ app.set("view engine", "handlebars");
 //link contoller
 var router = require("./controllers/burgers_controllers.js");
 app.use('/', router);
+//this is importing the css and images 
 
 //starts the server to began listening
 app.listen(PORT, function() {
